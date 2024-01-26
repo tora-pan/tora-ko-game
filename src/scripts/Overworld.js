47baa8df4d50a475ebfcd2f860f3a4c46df13d4f
tora-pan <travis.pandos@gmail.com>
2024-01-26 12:29:24 -0800

    add world maps

--- src/scripts/Overworld.js
+++ src/scripts/Overworld.js
@@ -3,9 +3,32 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector('.game-canvas');
         this.ctx = this.canvas.getContext('2d');
+        this.map = null;
+    }
+
+    startGameLoop() {
+        const step = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            // Draw layers
+            this.map.drawLower(this.ctx);
+            //draw game objects in between
+            Object.values(this.map.gameObjects).forEach((obj) => {
+                // obj.x += 0.02;
+                obj.sprite.draw(this.ctx);
+            });
+            this.map.drawUpper(this.ctx);
+
+            requestAnimationFrame(() => {
+                step();
+            });
+        };
+        step();
     }
 
     init() {
+        this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+        this.startGameLoop();
+
         const x = 5;
         const y = 6;
         const GRID_SIZE = 16;
